a854a518d4b0c70a09eff7e4d1e0f52653a37436
Jofred <reinosaj@gmail.com>
2025-05-08 12:10:20 -0400

    feat(users): implements initial service with get method

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,40 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {firstValueFrom, map} from "rxjs";
+import {environment} from "../../environments/environment";
+
+export interface User {
+  cellPhone: string;
+  dob: string;
+  email: string;
+  fullName: string;
+  thumbnail: string;
+  userName: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
+
+  getUsers(): Promise<User[]> {
+    const users = this.http.get<User[]>(`${this.apiUrl}?results=10&nat=gb`)
+      .pipe( map((data: any) => {
+        const response: User[] = data.results.map((user: any) => {
+          return {
+            cellPhone: user.cell,
+            dob: user.dob.date,
+            email: user.email,
+            fullName: `${user.name.first} ${user.name.last}`,
+            thumbnail: user.picture.thumbnail,
+            userName: user.login.username,
+          }
+        });
+        return response;
+      }));
+
+    return firstValueFrom(users);
+  }
+}
